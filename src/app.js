1d338db33ad63c9ff7e613f04b8073147f4f9327
sajan Limbu <limbusajan087@gmail.com>
2025-07-31 07:54:51 +0545

    fixing bugs

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use((req, res, next) => {
+  const contentType = req.headers["content-type"] || "";
+  if (contentType.startsWith("multipart/form-data")) {
+    return next();
+  }
+  express.json({ limit: "16kb" })(req, res, () => {
+    express.urlencoded({ extended: true, limit: "16kb" })(req, res, next);
+  });
+});
+app.use(express.static("public"));
+app.use(cookieParser());
 
+import userRouter from "./routes/user.routes.js";
 
-import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users", userRouter);
 
-app.use("/api/v1/users", userRouter)
-
-export {app}
+export { app };
